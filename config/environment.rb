fadf97f74212724784bc08e11063accf18da0eb7
Greg Eng <greg.eng@gmail.com>
2013-10-20 12:59:17 -0400

    add placeholder spec files for models

--- config/environment.rb
+++ config/environment.rb
@@ -5,9 +5,9 @@ require 'cgi'
 require 'httparty'
 require 'addressable/uri'
 
-
 require_relative '../lib/models/api'
+require_relative '../lib/models/correct_answer'
+require_relative '../lib/models/game'
+
+
 require_relative '../bin/cli'
-
-
-# require_relative '../spec/spec_helper'
---
+++ spec/game_spec.rb
@@ -0,0 +1,9 @@
+require_relative 'spec_helper.rb'
+
+describe Game do
+
+  it "does something"
+
+  it "does something else"
+
+end
