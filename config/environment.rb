b3b2168fc9f96cfc0125968d866dd6208f590fb6
Greg Eng <greg.eng@gmail.com>
2013-10-20 12:07:22 -0400

    build api class

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,12 @@
 require 'pry'
 require 'erb'
+require 'json'
+require 'cgi'
+require 'httparty'
+require 'addressable/uri'
 
-require_relative '../lib/models/artist'
+
+require_relative '../lib/models/api'
 
 
 # require_relative '../spec/spec_helper'
