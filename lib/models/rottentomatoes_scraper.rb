680d82748f3cbbf2f7c854ca638c9570e0396aed
Greg Eng <greg.eng@gmail.com>
2013-10-22 10:27:37 -0400

    rescue 404 pages on netflix scraper, regex for special characters in actor names

--- lib/models/rottentomatoes_scraper.rb
+++ lib/models/rottentomatoes_scraper.rb
@@ -8,7 +8,7 @@ class RottenTomatoesScraper
 
   def initialize(actor)
     begin
-    actor = actor.downcase.strip.gsub(" ","_")
+    actor = actor.downcase.strip.gsub(/[^a-z ]/,"").gsub(" ","_")
     url = TOMATOES + actor
     @scraped_data = Nokogiri::HTML(open(url))
     rescue
@@ -17,7 +17,12 @@ class RottenTomatoesScraper
   end
 
   def get_image
+    begin
     @scraped_data.css("div.media_block_image.big a img").attr("src").value
+    rescue
+      "This person isn't on RottenTomatoes and hence... not an A-Lister!"
+    end
+
   end
 
 end
